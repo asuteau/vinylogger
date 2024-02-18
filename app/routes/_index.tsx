9a75ec75e0e7a04aa2452fd50c63238e3c442a1d
Aymeric Suteau <aymeric.suteau.work@gmail.com>
2024-02-18 15:29:07 -0800

    chore: remove headers from index route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,25 +1,7 @@
 import { DiscogsClient } from '@lionralfs/discogs-client';
 import { LoaderFunctionArgs, json } from '@remix-run/node';
 import { Form, Link, useLoaderData } from '@remix-run/react';
-import { getUserIdentity, getUserProfile } from '~/services/discogs';
 import { getSession } from '~/sessions.server';
-
-export function headers({
-  loaderHeaders,
-  parentHeaders,
-}: {
-  loaderHeaders: Headers;
-  parentHeaders: Headers;
-}) {
-  console.log(
-    'This is an example of how to set caching headers for a route, feel free to change the value of 60 seconds or remove the header',
-  );
-  return {
-    // This is an example of how to set caching headers for a route
-    // For more info on headers in Remix, see: https://remix.run/docs/en/v1/route/headers
-    'Cache-Control': 'public, max-age=60, s-maxage=60',
-  };
-}
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   // Get storage session
