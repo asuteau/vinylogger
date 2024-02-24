25e56973796373dd21c4bdb2a11f232b40b62c37
Aymeric Suteau <aymeric.suteau.work@gmail.com>
2024-02-24 11:11:55 -0800

    fix(storage): set session lifetime to 6 months

--- app/sessions.server.ts
+++ app/sessions.server.ts
@@ -18,6 +18,7 @@ const { getSession, commitSession, destroySession } =
       path: "/",
       sameSite: "lax",
       secrets: ["s3cret1"],
+      maxAge: 356 * 24 * 60 * 60 // 1 year
     },
   });
 
