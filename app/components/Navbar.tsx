39dc38ea2583463ffbd28b450194fb935ef6e7e0
Aymeric Suteau <aymeric.suteau.work@gmail.com>
2024-06-03 17:56:41 +0200

    feat: improve navbar ui

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -17,10 +17,10 @@ const NavbarItem = ({label, icon, to}: MenuItem) => {
     <NavLink
       to={to}
       prefetch="intent"
-      className="flex flex-col md:flex-row justify-center items-center gap-2 md:gap-4 md:p-2 hover:cursor-pointer hover:bg-gray-700 hover:rounded-lg"
+      className="flex flex-col md:flex-row justify-center md:justify-start items-center gap-1 md:gap-4 md:px-8 md:py-4 md:border-l-4 border-transparent hover:cursor-pointer hover:bg-gray-800 hover:md:border-l-gray-100 h-full md:h-auto w-full font-bold text-gray-400"
     >
       {icon}
-      <span className=" text-gray-100">{label}</span>
+      <span>{label}</span>
     </NavLink>
   );
 };
@@ -29,22 +29,22 @@ const Navbar = () => {
   const menuItems: MenuItem[] = [
     {
       label: 'Home',
-      icon: <HouseLine weight="bold" size={24} className="text-gray-200" />,
-      to: '/',
+      icon: <HouseLine weight="bold" size={24} />,
+      to: '/dashboard',
     },
     {
       label: 'Collection',
-      icon: <Tag weight="bold" size={24} className="text-gray-200" />,
+      icon: <Tag weight="bold" size={24} />,
       to: '/collection',
     },
     {
       label: 'Wantlist',
-      icon: <Star weight="bold" size={24} className="text-gray-200" />,
+      icon: <Star weight="bold" size={24} />,
       to: '/wantlist',
     },
     {
       label: 'Profile',
-      icon: <User weight="bold" size={24} className="text-gray-200" />,
+      icon: <User weight="bold" size={24} />,
       to: '/profile',
     },
   ];
@@ -52,9 +52,9 @@ const Navbar = () => {
   return (
     <section
       id="navbar"
-      className="layout-navbar flex md:flex-col px-8 md:p-8 justify-between md:justify-start items-center md:items-start md:gap-4 shadow-inner md:shadow-none md:border-r md:border-gray-300 bg-gray-950"
+      className="layout-navbar flex md:flex-col md:pt-8 justify-between md:justify-start items-center md:items-start md:gap-4 shadow-inner md:shadow-none md:border-r md:border-gray-300 bg-gray-950"
     >
-      <Logo color="light" className="hidden md:flex my-8" />
+      <Logo color="light" className="hidden md:flex mx-8 my-6" />
       {menuItems.map((item) => (
         <NavbarItem key={item.label} {...item} />
       ))}
