dcbf7143d6f7cf6164b9b6748eb693768b7ddffb
Aymeric Suteau <aymeric.suteau.work@gmail.com>
2024-07-08 09:31:12 +0200

    feat(header): add navigation on logo

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,4 +1,5 @@
 import {VinylRecord} from '@phosphor-icons/react/dist/icons/VinylRecord';
+import {NavLink} from '@remix-run/react';
 import useMediaQuery from '~/hooks/use-media-query';
 type LogoProps = {
   className: string;
@@ -9,14 +10,14 @@ const Logo = ({className, color = 'dark'}: LogoProps) => {
   const isMobile = useMediaQuery();
 
   return (
-    <section id="logo" className={`flex gap-2 md:gap-4 items-center font-logo ${className}`}>
+    <NavLink id="logo" to="/dashboard" className={`flex gap-2 md:gap-4 items-center font-logo ${className} logo`}>
       <VinylRecord
         size={isMobile ? 32 : 48}
         weight="duotone"
         className={color === 'light' ? 'fill-slate-100' : 'fill-slate-900'}
       />
       <h1 className={color === 'light' ? 'text-slate-100' : 'text-slate-900'}>Vinylogger</h1>
-    </section>
+    </NavLink>
   );
 };
 
