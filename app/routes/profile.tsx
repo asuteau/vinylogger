b1d4975e2ab43b9364a1b5d74f971d4ef3659200
Aymeric Suteau <aymeric.suteau.work@gmail.com>
2024-07-13 17:51:55 +0200

    feat: update profile route

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,21 +1,48 @@
+import {json, LoaderFunctionArgs} from '@remix-run/node';
+import {useLoaderData} from '@remix-run/react';
 import UserSignOut from '~/components/UserSignOut';
 import {Button} from '~/components/ui/button';
+import {authenticator} from '~/services/auth.server';
 
 // Provides data to the component
-export const loader = async () => {
-  return null;
+export const loader = async ({request}: LoaderFunctionArgs) => {
+  const user = await authenticator.isAuthenticated(request, {
+    failureRedirect: '/',
+  });
+
+  return json({user});
 };
 
 // Renders the UI
-const ProfileRoute = () => (
-  <div className="flex w-full justify-center items-center md:hidden">
-    <Button asChild>
-      <UserSignOut />
-    </Button>
-  </div>
-);
+const ProfileRoute = () => {
+  const {user} = useLoaderData<typeof loader>();
 
-// Updates persistent data
-export const action = async () => {};
+  return (
+    <div className="flex flex-col md:hidden gap-4 text-xs h-full">
+      <div className="flex flex-col">
+        <span className="text-slate-950 font-bold">Id</span>
+        <span className="text-slate-600">{user.id}</span>
+      </div>
+      <div className="flex flex-col">
+        <span className="text-slate-950 font-bold">Username</span>
+        <span className="text-slate-600">{user.username}</span>
+      </div>
+      <div className="flex flex-col">
+        <span className="text-slate-950 font-bold">Consumer name</span>
+        <span className="text-slate-600">{user.consumerName}</span>
+      </div>
+      <div className="flex flex-col">
+        <span className="text-slate-950 font-bold">Resource URL</span>
+        <span className="text-slate-600">{user.resourceUrl}</span>
+      </div>
+
+      <div className="mt-auto self-center">
+        <Button asChild>
+          <UserSignOut />
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default ProfileRoute;
