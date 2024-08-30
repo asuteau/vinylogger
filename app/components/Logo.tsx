f58673318ff4a860bbe2e7cb9f58b708dd047fb6
Aymeric Suteau <aymeric.suteau.work@gmail.com>
2024-08-30 16:50:18 +0200

    feat: update logo

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,4 +1,3 @@
-import {VinylRecord} from '@phosphor-icons/react/dist/icons/VinylRecord';
 import {NavLink} from '@remix-run/react';
 import {useTheme} from '@/contexts/theme-context';
 import useMediaQuery from '@/hooks/use-media-query';
@@ -13,8 +12,8 @@ const Logo = ({className}: LogoProps) => {
   const isLight = theme === 'light';
 
   return (
-    <NavLink id="logo" to="/" className={`flex gap-2 items-center font-logo ${className} logo`}>
-      <VinylRecord size={isMobile ? 32 : 48} weight="duotone" className="fill-sky-700" />
+    <NavLink id="logo" to="/" className={`flex gap-4 md:gap-6 items-center font-logo ${className} logo`}>
+      <img className="h-10 md:h-16 aspect-square rounded-full md:rounded-xl" src="/img/logo.png" alt="logo" />
       <h1 className="font-['BricolageGrotesque'] text-slate-900 dark:text-slate-50">Vinylogger</h1>
     </NavLink>
   );
