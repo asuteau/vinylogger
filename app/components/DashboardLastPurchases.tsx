46042848377573ce6900797e9ac96fb99c3ecb13
Aymeric Suteau <aymeric.suteau.work@gmail.com>
2024-09-28 18:22:47 +0200

    chore: ui

--- app/components/DashboardLastPurchases.tsx
+++ app/components/DashboardLastPurchases.tsx
@@ -77,8 +77,8 @@ const DashboardLastPurchases = ({lastPurchases}: DashboardLastPurchasesProps) =>
         <span className="text-base text-muted-foreground line-clamp-1">{currentRelease.artist}</span>
         <div className="flex gap-2 items-center">
           <span className="relative flex h-3 w-3">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-primary"></span>
           </span>
           <span className="text-xs text-muted-foreground line-clamp-1">{currentRelease.addedOn}</span>
         </div>
